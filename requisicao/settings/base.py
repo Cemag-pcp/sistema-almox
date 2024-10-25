b7bce093352e19457411cd2663c4a1f3fc35a58e
luan <luan640@gmail.com>
2024-10-25 09:53:42 -0300

    middleware

--- requisicao/settings/base.py
+++ requisicao/settings/base.py
@@ -30,6 +30,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
 ]
 
 ROOT_URLCONF = 'requisicao.urls'
